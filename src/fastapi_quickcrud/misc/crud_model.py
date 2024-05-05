c97bb19a55a3d584f9b48cccd838bd0f8551367c
root <root@collector.bontekoe.technology>
2024-05-05 09:03:03 +0200

    Fix

--- src/fastapi_quickcrud/misc/crud_model.py
+++ src/fastapi_quickcrud/misc/crud_model.py
@@ -3,7 +3,7 @@ from typing import (Optional,
                     List)
 
 from pydantic import BaseModel
-from pydantic.main import ModelMetaclass
+from pydantic._internal._model_construction import ModelMetaclass
 
 from .exceptions import (RequestMissing,
                          InvalidRequestMethod)
